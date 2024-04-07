a7e488eb42bbf360a21c2a98207921745f6aa142
FlyingCuteCats <eliotcore@gmail.com>
2024-04-07 19:10:25 +0100

    Update Process.r
    Update codes for the US yield analysis

--- Gold Price Analysis/Process.r
+++ Gold Price Analysis/Process.r
@@ -29,9 +29,9 @@ for (i in 1:6) {
 ggplot(prices, aes(Date, US.10y.yield - CN.10y.yield, colour = factor(year(Date)))) + 
   geom_line()
 
-# Gold
+# Gold and US-CN yield
 
-plot_gold_price <- function(data, year, by_month = FALSE) {  
+plot_gold_price_yield <- function(data, year, by_month = FALSE) {  
   # first pick out the year  
   filtered_data <- data |> 
     filter(year(Date) == year)
@@ -41,7 +41,7 @@ plot_gold_price <- function(data, year, by_month = FALSE) {
     ggplot(aes(x = US.10y.yield - CN.10y.yield, y = Gold)) +
     geom_smooth() +
     geom_point(alpha = 0.5) +
-    ggtitle(paste("Gold Price in Year", year)) +
+    ggtitle(paste("Gold Price in Year", year, "(US-CN spread)")) +
     labs(x = "US-CN 10y spread")
   
   # decide which variant to plot   
@@ -56,9 +56,43 @@ plot_gold_price <- function(data, year, by_month = FALSE) {
   print(p)
 }
 
-plot_gold_price(prices, 2024, by_month = F)
-plot_gold_price(prices, 2024, by_month = T)
-plot_gold_price(prices, 2023, by_month = F)
-plot_gold_price(prices, 2023, by_month = T)
-plot_gold_price(prices, 2022, by_month = F)
-plot_gold_price(prices, 2022, by_month = T)
+# gold price and US yield
+
+plot_gold_price_us <- function(data, year, by_month = FALSE) {  
+  # first pick out the year  
+  filtered_data <- data |> 
+    filter(year(Date) == year)
+  
+  # then generate the plot  
+  p <- filtered_data |> 
+    ggplot(aes(x = US.10y.yield, y = Gold)) +
+    geom_smooth() +
+    geom_point(alpha = 0.5) +
+    ggtitle(paste("Gold Price in Year", year, "(US yield)")) +
+    labs(x = "US yield")
+  
+  # decide which variant to plot   
+  if (!by_month) {
+    p <- p + aes(colour = factor(month(Date))) +
+      labs(colour = "Month")
+  } else {
+    p <- p + aes(colour = day(Date)) +
+      facet_wrap(~month(Date)) +
+      labs(colour = "Day")
+  }  
+  print(p)
+}
+
+plot_gold_price_yield(prices, 2024, by_month = F)
+plot_gold_price_yield(prices, 2024, by_month = T)
+plot_gold_price_yield(prices, 2023, by_month = F)
+plot_gold_price_yield(prices, 2023, by_month = T)
+plot_gold_price_yield(prices, 2022, by_month = F)
+plot_gold_price_yield(prices, 2022, by_month = T)
+
+plot_gold_price_us(prices, 2024, by_month = F)
+plot_gold_price_us(prices, 2024, by_month = T)
+plot_gold_price_us(prices, 2023, by_month = F)
+plot_gold_price_us(prices, 2023, by_month = T)
+plot_gold_price_us(prices, 2022, by_month = F)
+plot_gold_price_us(prices, 2022, by_month = T)
